dd75aa62ce2b9fcc842deb4fcbe1b6d2e7db9305
Ivan Kasenkov <kacenkov7@gmail.com>
2021-10-01 22:11:05 +0300

    refactor

--- src/router/useBehaviourSubject.ts
+++ src/router/useBehaviourSubject.ts
@@ -1,11 +1,10 @@
 import { BehaviourSubject } from '@keiii/k-stream';
 import { Reducer, useLayoutEffect, useReducer } from 'react';
 
+const reducer = <T>(_: T, x: T) => x;
+
 export const useBehaviourSubject = <T>(s: BehaviourSubject<T>): T => {
-  const [value, next] = useReducer<Reducer<T, T>>(
-    (_, action) => action,
-    s.getValue(),
-  );
+  const [value, next] = useReducer<Reducer<T, T>>(reducer, s.getValue());
 
   useLayoutEffect(() => s.subscribe({ next }).unsubscribe, [s]);
 
